4d4db660e337418176981ef5ef043b61f9e8ae6f
Alextxnk <alexsolov766@gmail.com>
2023-08-11 14:13:34 +0300

    add new scripts

--- lesson4/lesson4.js
+++ lesson4/lesson4.js
@@ -1,2 +1,90 @@
 "use strict";
-// Generic
+// Generics - обобщенный тип
+// благодаря им мы можем делать универсальные функции с точки зрения типов
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// встроенные Generics
+const array = [1, 2, 3];
+function testFunc() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const promise = yield new Promise((resolve, reject) => {
+            resolve(1);
+        });
+    });
+}
+// Record означает, что у нас могут находиться свойства типов string и boolean
+const record = {
+    drive: true,
+    kpp: false
+};
+// функция с Generic
+// c их помощью мы можем сделать обобщенную универсальную функцию,
+// которая будет работать с любыми типами, которые мы передадим
+function logMiddleware(data) {
+    console.log(data);
+    return data;
+}
+const logRes = logMiddleware(10);
+// еще пример функции с Generic
+function getSplittedHalf(data) {
+    const l = data.length / 2;
+    return data.splice(0, l);
+}
+const resSplit = getSplittedHalf([1, 3, 2, 4, 7, 5]);
+console.log('resSplit', resSplit);
+// функция преобразования в строку
+// написать функцию toString, которая принимает любой тип и возвращает его строковое представление,
+// если не может, то возвращает undefined
+function toString(data) {
+    if (Array.isArray(data)) {
+        return data.toString();
+    }
+    switch (typeof data) {
+        case 'string':
+            return data;
+        case 'number':
+        case 'symbol':
+        case 'bigint':
+        case 'boolean':
+        case 'function':
+            return data.toString();
+        case 'object':
+            return JSON.stringify(data);
+        default:
+            return undefined;
+    }
+}
+console.log('str', toString(['data', 'str']));
+console.log('str', toString(true));
+console.log('str', toString(5));
+console.log('str', toString({ a: 'data', b: 'str' }));
+console.log('str', toString(getSplittedHalf));
+// использование в типах
+const split = getSplittedHalf;
+const logLine = {
+    timeStamp: new Date(),
+    data: {
+        a: 1
+    }
+};
+// ограничение Generic
+class VehicleC {
+}
+// мы также можем extends типы и интерфейсы
+function kmToMiles(vehicle) {
+    vehicle.run = vehicle.run / 0.62;
+    return vehicle;
+}
+class LCV extends VehicleC {
+}
+const vehicle = kmToMiles(new VehicleC());
+const lcv = kmToMiles(new LCV());
+kmToMiles({ run: 120 });
+function logIdFunc(id) { }
--- lesson4/lesson4.ts
+++ lesson4/lesson4.ts
@@ -4,7 +4,7 @@
 // встроенные Generics
 const array: Array<number> = [1, 2, 3];
 
-async function test() {
+async function testFunc() {
    const promise = await new Promise<number>((resolve, reject) => {
       resolve(1);
    });
@@ -32,4 +32,76 @@ function getSplittedHalf<T>(data: Array<T>): Array<T> {
    return data.splice(0, l);
 }
 
-getSplittedHalf<number>([1, 3, 2, 4, 7, 5]);
+const resSplit = getSplittedHalf<number>([1, 3, 2, 4, 7, 5]);
+console.log('resSplit', resSplit);
+
+// функция преобразования в строку
+// написать функцию toString, которая принимает любой тип и возвращает его строковое представление,
+// если не может, то возвращает undefined
+function toString<T>(data: T): string | undefined {
+   if (Array.isArray(data)) {
+      return data.toString();
+   }
+
+   switch (typeof data) {
+      case 'string':
+         return data;
+      case 'number':
+      case 'symbol':
+      case 'bigint':
+      case 'boolean':
+      case 'function':
+         return data.toString();
+      case 'object':
+         return JSON.stringify(data);
+      default:
+         return undefined;
+   }
+}
+
+console.log('str', toString(['data', 'str']));
+console.log('str', toString(true));
+console.log('str', toString(5));
+console.log('str', toString({ a: 'data', b: 'str' }));
+console.log('str', toString(getSplittedHalf));
+
+// использование в типах
+const split: <T>(data: Array<T>) => Array<T> = getSplittedHalf;
+
+interface ILogLine<T> {
+   timeStamp: Date;
+   data: T;
+}
+
+type LogLineType<T> = {
+   timeStamp: Date;
+   data: T;
+};
+
+const logLine: ILogLine<{ a: number }> = {
+   timeStamp: new Date(),
+   data: {
+      a: 1
+   }
+};
+
+// ограничение Generic
+class VehicleC {
+   run: number;
+}
+
+// мы также можем extends типы и интерфейсы
+function kmToMiles<T extends VehicleC>(vehicle: T): T {
+   vehicle.run = vehicle.run / 0.62;
+   return vehicle;
+}
+
+class LCV extends VehicleC {
+   capacity: number;
+}
+
+const vehicle = kmToMiles(new VehicleC());
+const lcv = kmToMiles(new LCV());
+kmToMiles({ run: 120 });
+
+function logIdFunc<T extends string | number>(id: T) {}
